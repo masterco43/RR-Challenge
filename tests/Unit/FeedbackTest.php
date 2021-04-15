9c07c11218c5063f8468756108d246c63eb27d12
Mfc23 <masterco43@gmail.com>
2021-04-15 17:18:53 -0400

    Updated tests to remove non needed querys

--- tests/Unit/FeedbackTest.php
+++ tests/Unit/FeedbackTest.php
@@ -18,77 +18,69 @@ class FeedbackTest extends TestCase
 
     public function testADD()
     {
-        $original = Feedback::count();
         $response = $this->post('/send_feedback', [
             'name' => 'testing',
             'email' => 'unit@testing.com',
             'comments' => 'This is a unit test'
         ]);
 
-        //echo($response);
-
-        $this->assertGreaterThan($original, Feedback::count(), "No new feedback added");
+        $this->assertEquals(1, Feedback::count(), "No new feedback added");
     }
 
     public function testShortName()
     {
-        $original = Feedback::count();
         $reaponse = $this->post('/send_feedback', [
             'name' => 't',
             'email' => 'unit@testing.com',
             'comments' => 'This is a unit test'
         ]);
 
-        $this->assertEquals(Feedback::count(), $original,  "New short name feedback added");
+        $this->assertEquals(Feedback::count(), 0,  "New short name feedback added");
     }
 
     public function testLongName()
     {
-        $original = Feedback::count();
         $reaponse = $this->post('/send_feedback', [
             'name' => 'testwaytootestwaytootestwaytootestwaytootestwaytootestwaytootestwaytoolong',
             'email' => 'unit@testing.com',
             'comments' => 'This is a unit test'
         ]);
 
-        $this->assertEquals(Feedback::count(), $original,  "New Long name feedback added");
+        $this->assertEquals(Feedback::count(), 0,  "New Long name feedback added");
     }
 
     public function testBadEmail()
     {
-        $original = Feedback::count();
         $reaponse = $this->post('/send_feedback', [
             'name' => 'validName',
             'email' => 'unit@testin',
             'comments' => 'This is a unit test'
         ]);
 
-        $this->assertEquals(Feedback::count(), $original,  "New bad email feedback added");
+        $this->assertEquals(Feedback::count(), 0,  "New bad email feedback added");
     }
 
     public function testShortComment()
     {
-        $original = Feedback::count();
         $reaponse = $this->post('/send_feedback', [
             'name' => 'validName',
             'email' => 'unit@testin',
             'comments' => 'T'
         ]);
 
-        $this->assertEquals(Feedback::count(), $original,  "New short comment feedback added");
+        $this->assertEquals(Feedback::count(), 0,  "New short comment feedback added");
     }
 
     public function testLongComment()
     {
         $comment = 'testwaytootestwaytootestwaytootestwaytootestwaytootestwaytootestwaytoolong';
-        $original = Feedback::count();
         $reaponse = $this->post('/send_feedback', [
             'name' => 'validName',
             'email' => 'unit@testin',
             'comments' => $comment.$comment.$comment.$comment.$comment.$comment
         ]);
 
-        $this->assertEquals(Feedback::count(), $original,  "New long comment feedback added");
+        $this->assertEquals(Feedback::count(), 0,  "New long comment feedback added");
     }
 
     public function testMultipleEmail(){
@@ -103,15 +95,14 @@ class FeedbackTest extends TestCase
             'comments' => 'This is a unit test'
         ]);
 
-        //gets count after the first valid 2 are sent in
-        $original = Feedback::count();
 
+        //should not add to db
         $reaponse = $this->post('/send_feedback', [
             'name' => 'testing',
             'email' => 'multiunit@testing.com',
             'comments' => 'This is a unit test'
         ]);
 
-        $this->assertEquals($original, Feedback::count(), "Third email feedback added");
+        $this->assertEquals(2, Feedback::count(), "Third email feedback added");
     }
 }
