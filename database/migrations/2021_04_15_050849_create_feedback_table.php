00e1e54ac88786d6edadc94d41e2fc1157d74d63
Mfc23 <masterco43@gmail.com>
2021-04-15 01:18:53 -0400

    Added in model and migrations

---
+++ database/migrations/2021_04_15_050849_create_feedback_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedbackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->char('name', 50);
+            $table->char('email', 80);
+            $table->char('comments', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedback');
+    }
+}
